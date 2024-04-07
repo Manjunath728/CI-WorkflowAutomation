61d451beeca5844c992c0b67dc258c65a4be9791
Manjunath B <manjunathb.mca22@rvce.edu.in>
2024-04-07 11:29:10 +0530

    Updated Ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css'; // Import CSS file for styling
-
+// App.js
+import  { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css"; // Import CSS file for styling
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTrash,
+  faEdit,
+  faCheck,
+  faTimes,
+  faPlus
+} from "@fortawesome/free-solid-svg-icons";
+import { Bounce, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   const [persons, setPersons] = useState([]);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [updatingId, setUpdatingId] = useState(null);
+  const [updatedName, setUpdatedName] = useState("");
+  const [updatedEmail, setUpdatedEmail] = useState("");
+  const [showDialog, setShowDialog] = useState(false);
 
   useEffect(() => {
     fetchPersons();
@@ -13,21 +27,37 @@ function App() {
 
   const fetchPersons = async () => {
     try {
-      const response = await axios.get('/api/person');
+      const response = await axios.get("/api/person");
       setPersons(response.data);
     } catch (error) {
-      console.error('Error fetching persons:', error);
+      console.error("Error fetching persons:", error);
     }
   };
 
   const addPerson = async () => {
+    if (!name || !email) {
+      // If either name or email is empty, show a toast notification
+      toast.error('Add both name and Email', {
+        position: "bottom-left",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+        });
+      return;
+    }
     try {
-      await axios.post('/api/person', { name, email });
+      await axios.post("/api/person", { name, email });
       fetchPersons();
-      setName('');
-      setEmail('');
+      setName("");
+      setEmail("");
+      setShowDialog(false); // Hide the dialog after adding person
     } catch (error) {
-      console.error('Error adding person:', error);
+      console.error("Error adding person:", error);
     }
   };
 
@@ -36,48 +66,154 @@ function App() {
       await axios.delete(`/api/person/${id}`);
       fetchPersons();
     } catch (error) {
-      console.error('Error deleting person:', error);
+      console.error("Error deleting person:", error);
     }
   };
 
-  const updatePerson = async (id, updatedName, updatedEmail) => {
+  const toggleUpdate = (id, currentName, currentEmail) => {
+    setUpdatingId(id);
+    setUpdatedName(currentName);
+    setUpdatedEmail(currentEmail);
+  };
+
+  const updatePerson = async (id) => {
+    if (!updatedName || !updatedEmail) {
+      // If either name or email is empty, show a toast notification
+      toast.error('Add both name and Email', {
+        position: "bottom-left",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+        });
+      return;
+    }
     try {
-      await axios.put(`/api/person/${id}`, { name: updatedName, email: updatedEmail });
+      await axios.put(`/api/person/${id}`, {
+        name: updatedName,
+        email: updatedEmail,
+      });
       fetchPersons();
+      setUpdatingId(null);
     } catch (error) {
-      console.error('Error updating person:', error);
+      console.error("Error updating person:", error);
     }
   };
 
   return (
     <div className="container">
-      <h1 className="heading">Manage Persons Microservice</h1>
-      <div className="form">
-        <input className="input" type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-        <input className="input" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <button className="button" onClick={addPerson}>Add Person</button>
+    <ToastContainer />
+      <h1 className="heading">People Manager Microservice</h1>
+     
+
+      <div className="cards-container">
+      
+        {persons.map((person) => (
+          <div className="card" key={person.id}>
+            <div className="card-body">
+              {updatingId === person.id ? (
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Updated Name"
+                  value={updatedName}
+                  onChange={(e) => setUpdatedName(e.target.value)}
+                />
+              ) : (
+                <h3>{person.name}</h3>
+              )}
+              {updatingId === person.id ? (
+                <input
+                  className="input"
+                  type="email"
+                  placeholder="Updated Email"
+                  value={updatedEmail}
+                  onChange={(e) => setUpdatedEmail(e.target.value)}
+                />
+              ) : (
+                <p>{person.email}</p>
+              )}
+            </div>
+            <div className="card-footer">
+              {updatingId === person.id ? (
+                <div className="action-buttons">
+                  <button
+                    className="ok-button"
+                    onClick={() => {
+                      updatePerson(person.id);
+                      setUpdatingId(null);
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faCheck} />
+                  </button>
+                  <button
+                    className="cancel-button"
+                    onClick={() => setUpdatingId(null)}
+                  >
+                    <FontAwesomeIcon icon={faTimes} />
+                  </button>
+                </div>
+              ) : (
+                <>
+                  <button
+                    className="delete-button"
+                    onClick={() => deletePerson(person.id)}
+                  >
+                    <FontAwesomeIcon icon={faTrash} />
+                  </button>
+                  <button
+                    className="update-button"
+                    onClick={() =>
+                      toggleUpdate(person.id, person.name, person.email)
+                    }
+                  >
+                    <FontAwesomeIcon icon={faEdit} />
+                  </button>
+                </>
+              )}
+              
+            </div>
+           
+          </div>
+        ))}
+        {showDialog && (
+          <div className="overlay">
+            <div className="card dialog">
+              <div className="card-body">
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                  className="input"
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="card-footer">
+                <button className="button" onClick={addPerson}>
+                  Add Person
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {persons.map(person => (
-            <tr key={person.id}>
-              <td>{person.name}</td>
-              <td>{person.email}</td>
-              <td>
-                <button className="delete-button" onClick={() => deletePerson(person.id)}>Delete</button>
-                <button className="update-button" onClick={() => updatePerson(person.id, prompt('Enter updated name'), prompt('Enter updated email'))}>Update</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <button
+        className="floating-button"
+        onClick={() => setShowDialog(true)}
+      >
+        <FontAwesomeIcon icon={faPlus} />
+      </button>
     </div>
   );
 }
